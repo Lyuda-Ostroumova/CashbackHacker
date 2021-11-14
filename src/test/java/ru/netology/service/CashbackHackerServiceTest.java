949089e377b0520b8578715acd323632d07ac3ce
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-14 15:25:03 +0300

    Test with TestNG

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,68 +1,67 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashbackHackerServiceTest {
+public class CashbackHackerServiceTest {
 
     @Test
-    void shouldTellWhatRemains() {
+    public void shouldTellWhatRemains() {
 
         CashbackHackerService cashbackService = new CashbackHackerService();
 
         int actual = cashbackService.remain(900);
         int expected = 100;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
     @Test
-    void shouldTellWhatRemainsIfSumIs1000() {
+    public void shouldTellWhatRemainsIfSumIs1000() {
 
         CashbackHackerService cashbackService = new CashbackHackerService();
 
         int actual = cashbackService.remain(1000);
         int expected = 0;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
     @Test
-    void shouldTellWhatRemainsIfSumIs0() {
+    public void shouldTellWhatRemainsIfSumIs0() {
 
         CashbackHackerService cashbackService = new CashbackHackerService();
 
         int actual = cashbackService.remain(0);
         int expected = 1000;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
     @Test
-    void shouldTellWhatRemainsIfSumIs500() {
+    public void shouldTellWhatRemainsIfSumIs500() {
 
         CashbackHackerService cashbackService = new CashbackHackerService();
 
         int actual = cashbackService.remain(500);
         int expected = 500;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
     @Test
-    void shouldTellWhatRemainsIfSumExceedsThousand() {
+    public void shouldTellWhatRemainsIfSumExceedsThousand() {
 
         CashbackHackerService cashbackService = new CashbackHackerService();
 
         int actual = cashbackService.remain(1400);
         int expected = 600;
 
-        assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
 
     }
 
