ec9093f2b859a57ac961a7ff32baf412f8b5a801
Lyudmila Ostroumova <lyudmila.ostroumova@gmail.com>
2021-11-14 14:01:52 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackerServiceTest {
+
+    @Test
+    void shouldTellWhatRemains() {
+
+        CashbackHackerService cashbackService = new CashbackHackerService();
+
+        int actual = cashbackService.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldTellWhatRemainsIfSumIs1000() {
+
+        CashbackHackerService cashbackService = new CashbackHackerService();
+
+        int actual = cashbackService.remain(1000);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldTellWhatRemainsIfSumIs0() {
+
+        CashbackHackerService cashbackService = new CashbackHackerService();
+
+        int actual = cashbackService.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldTellWhatRemainsIfSumIs500() {
+
+        CashbackHackerService cashbackService = new CashbackHackerService();
+
+        int actual = cashbackService.remain(500);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void shouldTellWhatRemainsIfSumExceedsThousand() {
+
+        CashbackHackerService cashbackService = new CashbackHackerService();
+
+        int actual = cashbackService.remain(1400);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+}
